1e8e439689504f94bcbed3dd9b1292d77a22a972
Dhini <dinnyas@gmail.com>
2020-09-29 00:48:03 +0800

    tambah file coba php statis

---
+++ try2th/coba1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Barang</title>
+</head>
+
+<body>
+  <h3>Daftar Barang</h3>
+
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Kode Barang</th>
+      <th>Nama Barang</th>
+      <th>Jenis Barang</th>
+      <th>Lokasi</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td> <img src="img/map.jpg" width="60"></td>
+      <td>KD00001</td>
+      <td>Peta</td>
+      <td>Inventaris</td>
+      <td>Ruang Subbag Umum</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+
+  </table>
+
+</body>
+
+</html>
